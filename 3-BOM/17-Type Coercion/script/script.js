364e72beb3ef31214301d82b41c3112bf2edd86f
arminabdijs <arminabdijs@gmail.com>
2026-06-10 01:49:25 +0330

    Add type coercion example

---
+++ 3-BOM/17-Type Coercion/script/script.js
@@ -0,0 +1,23 @@
+// Type Coercion (Implicit - Explicit)
+
+/* let userAge = prompt("Enter your age:", 18);
+
+console.log(`User Age : ${userAge}`);
+console.log(`User Age type : ${typeof userAge}`); */
+
+//----------------------------------------------------
+
+/* let userAge = Number(prompt("Enter your age:", 18));
+
+console.log(`User Age : ${userAge}`);
+console.log(`User Age type : ${typeof userAge}`);
+ */
+
+//----------------------------------------------------
+console.log(true + false);
+
+console.log(true + true);
+
+console.log(12 / "3");
+
+console.log(12 == "12");
