83a95512e268f7fb427eb03ab1c774e3b8573363
arminabdijs <arminabdijs@gmail.com>
2026-09-05 02:31:42 +0330

    up

--- 2-DOM/29-unload and beforeUnLoad/script/script.js
+++ 2-DOM/29-unload and beforeUnLoad/script/script.js
@@ -1,20 +1,35 @@
 let $ = document;
 let { log } = console;
 
-window.addEventListener('unload', () => {
+/* window.addEventListener('unload', () => {
   log('event unloaded');
 });
 
 function unloadHandler() {
   log('unloaded');
-}
+} */
 
+// ======================================================
+window.addEventListener('pagehide', () => {
+  alert('کاربر صفحه را ترک کرد');
+});
 // ======================================================
 
 window.addEventListener('beforeunload', () => {
-  log('event beforeunload');
+  alert('event beforeunload');
 });
 
 function beForeunLoadHandler() {
   log('event beforeunload');
 }
+
+
+/*
+کاربر می‌خواهد صفحه را ترک کند
+          ↓
+     beforeunload
+          ↓
+      pagehide
+          ↓
+       صفحه خارج شد
+ */
